68d33bed091ec07e7bb2b64415f5981d3f8b203c
mspronesti <massimiliano.pronesti@gmail.com>
2022-10-30 19:23:48 +0100

    feat: add autoencoder and attention
    * implement encoder and decoder
    * implement self and cross attention

---
+++ README.md
@@ -0,0 +1,17 @@
+# Stable Diffusion in PyTorch [NO Token Required]
+
+Implementation of Stable Diffusion in PyTorch, primarily following the implementation
+proposed in [this](https://github.com/divamgupta/stable-diffusion-tensorflow) repository, for personal usage and learning purpose.
+
+The weights were ported from the original implementation and made pytorch-compatible. No huggingface token is required for this
+implementation.
+
+Other consulted repositories
+- [CompVis/stable-diffusion](https://github.com/CompVis/stable-diffusion)
+- [huggingface/transformers](https://github.com/huggingface/transformers)
+- [huggingface/diffusers](https://github.com/huggingface/diffusers)
+- [AUTOMATIC1111/stable-diffusion-webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui)
+
+## Colab Notebooks
+
+## Usage
---
+++ stable_diffusion_pt/ae/encoder.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+from .decoder import AttentionBlock, ResidualBlock
+
+
+class Encoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
+            ResidualBlock(128, 128),
+            ResidualBlock(128, 128),
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            ResidualBlock(128, 256),
+            ResidualBlock(256, 256),
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
+            ResidualBlock(256, 512),
+            ResidualBlock(512, 512),
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+            ResidualBlock(512, 512),
+            ResidualBlock(512, 512),
+            ResidualBlock(512, 512),
+            AttentionBlock(512),
+            ResidualBlock(512, 512),
+            nn.GroupNorm(32, 512),
+            nn.SiLU(),
+            nn.Conv2d(512, 8, kernel_size=3, padding=1),
+            nn.Conv2d(8, 8, kernel_size=1, padding=0),
+        )
+
+    def forward(self, x, noise):
+        x = super().forward(x)
+
+        mean, log_variance = torch.chunk(x, 2, dim=1)
+        log_variance = torch.clamp(log_variance, -30, 20)
+        variance = log_variance.exp()
+        stdev = variance.sqrt()
+        x = mean + stdev * noise
+
+        x *= 0.18215
+        return x
